f83457b1175cec53aec3e4106774859deef9bf46
unknown <jaykulkarni17ab@gmail.com>
2023-05-30 09:56:18 +0530

    Mern-app

---
+++ dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect(
+  "mongodb+srv://jayeshkulkarni1703:17072003@cluster0.kteok1g.mongodb.net/",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+
+connection.on("error", (err) => console.log(err));
+
+connection.on("connected", () =>
+  console.log("Mongo DB Connection Successfull")
+);
